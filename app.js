00e1f88bf4c107377490f5a5298bdb317598c89a
Parimal <parimal.ingle18@gmail.com>
2021-04-04 04:32:49 +0530

    Till certifications page done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects.ejs");
+});
+
+app.get("/certifications", (req, res) => {
+    res.render("certifications.ejs");
+});
+
+app.listen(3000, () => {
+    console.log("App is Working!!!");
+});
